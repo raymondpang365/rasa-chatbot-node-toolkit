9b2164df545f32991c75fb143b8d52daf120b187
psfr937 <psfr937@gmail.com>
2018-06-12 10:24:09 +0800

    fixed oauth callback url

--- src/config/passportStrategy/google/credential.js
+++ src/config/passportStrategy/google/credential.js
@@ -2,11 +2,11 @@ module.exports = {
   development: {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: `http://${process.env.NODE_HOST}:3000/auth/google/callback`
   },
   production: {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:8080/auth/google/callback'
+    callbackURL: `http://${process.env.NODE_HOST}:8080/auth/google/callback`
   }
 };
