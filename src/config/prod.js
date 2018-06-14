ef8eef0bf885e0a809c271940a4a0cffdf986e44
psfr937 <psfr937@gmail.com>
2018-06-14 15:39:15 +0800

    change publicPort to 8080

--- src/config/prod.js
+++ src/config/prod.js
@@ -7,7 +7,7 @@ const defaultConfig = require('./default');
 module.exports = merge(defaultConfig, {
   host: process.env.NODE_HOST || 'localhost', // Define your host from 'package.json'
   port: process.env.PORT || 8080,
-  publicPort: process.env.PUBLIC_PORT || 80,
+  publicPort: process.env.PUBLIC_PORT || 8080,
   sslEnabled: process.env.SSL_ENABLED,
   passportStrategy: {
     facebook: require('./passportStrategy/facebook/credential').production,
